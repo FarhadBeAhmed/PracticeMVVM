1129b4829ec99c384d209549ed6b65e60b6f84ca
Md. Farhad Hossain <50591727+FarhadBeAhmed@users.noreply.github.com>
2023-04-15 09:28:10 -0700

    Update MainActivity.java

--- app/src/main/java/com/practicemvvm/MainActivity.java
+++ app/src/main/java/com/practicemvvm/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 String newVer;
+    String newVar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
